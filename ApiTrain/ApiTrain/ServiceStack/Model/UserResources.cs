c8abc8a6142415c4cb52580db9fcdad7de0e3d98
Zsolt Schoner <zsolt_schoner@epam.com>
2012-11-22 19:22:40 +0100

    Code cleanup #3

---
+++ ApiTrain/ApiTrain/ServiceStack/Model/UserResources.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using ServiceStack.ServiceHost;
+using Common.Model;
+
+namespace ServiceStack.Model
+{
+    #region User request contract
+
+    /// <summary>
+    /// Defines a resource for list handling
+    /// </summary>
+    [Route("api/user", "GET")]
+    public class UserListResource
+    {
+    }
+
+    /// <summary>
+    /// Defines a resource for User handling
+    /// 
+    /// When you use Route attribute, you need set the Content-Type header to a proper
+    /// value, for example for JSON requests "Content-Type: application/json"
+    /// otherwise routing will remove JSON content from the request body
+    /// Other problem can be the invalid Data-Type header settings like in this post
+    /// http://stackoverflow.com/questions/11678610/why-servicestack-could-not-make-model-binding-on-json-post-request
+    /// </summary>
+    [Route("api/user/{id}", "GET")]
+    [Route("api/user", "POST")]
+    [Route("api/user", "PUT")]
+    [Route("api/user/{id}", "DELETE")]
+    [Route("api/user/{id}/{UserName}/{Name}/{IsAnonymous}", "PUT")]
+    public class UserResouce : UserModel { }
+
+    #endregion
+}
